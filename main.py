320fdbfde928bb2654eae7122d7d33def9f7a31f
justlink10 <bart.marius.de.vries@gmail.com>
2024-01-11 14:56:58 +0100

    install pre-commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import numpy as np
-print("Hello users! Understanding git is difficult, but when you are familiar with git you do not want to use anything else. Good luck")
-print("We also implemented some hooks (black and flake8) to harmonize coding for multiple users. This helps to improve readability!")
+
+print(
+    "Hello users! Understanding git is difficult, but when you are familiar with git you do not want to use anything else. Good luck"
+)
+print(
+    "We also implemented some hooks (black and flake8) to harmonize coding for multiple users. This helps to improve readability!"
+)
+print(
+    "We also implemented some hooks (black and flake8) to harmonize coding for multiple users. This helps to improve readability!"
+)
+VAR = 'help'
