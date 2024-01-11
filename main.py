1976de70c031e8bcac3656d629b55de6edfca58d
justlink10 <bart.marius.de.vries@gmail.com>
2024-01-11 11:00:33 +0100

    Updated main and added hooks

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import numpy as np
 print("Hello users! Understanding git is difficult, but when you are familiar with git you do not want to use anything else. Good luck")
+print("We also implemented some hooks (black and flake8) to harmonize coding for multiple users. This helps to improve readability!")
